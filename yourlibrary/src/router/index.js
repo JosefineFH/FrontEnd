dbc1ab4dc4f3308cdb0f2f3cdab2a7329367d95c
Josefine Magnussen <josefine.magnussen@hotmail.com>
2020-04-04 01:06:25 +0200

    router updated

--- yourlibrary/src/router/index.js
+++ yourlibrary/src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Main from '@/views/main.vue'
+import Books from '@/views/yourbooks.vue'
+import Login from '@/views/login.vue'
+import AddBook from '@/views/AddBook'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+      path: "/",
+      name: "Main",
+      component: Main,
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login,
+    },
+    {
+      path: "/books",
+      name: "Books",
+      component: Books,
+    },
+    {
+      path: "/add",
+      name: "AddBook",
+      component: AddBook,
+    },
+  ],
+});
