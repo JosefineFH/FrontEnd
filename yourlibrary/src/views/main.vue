8b73b1f1e455077151fc77fe2db0de6b6d6cd6c4
Josefine Magnussen <josefine.magnussen@hotmail.com>
2020-04-04 01:05:33 +0200

    Main page

---
+++ yourlibrary/src/views/main.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>Welcome</p>
+  </div>
+</template>
+
+<script>
+
+</script>
+<style scoped>
+input {
+    margin: 2px;
+}
+form {
+    text-align: center;
+    padding-top: 60px;
+}
+.button {
+    border: none;
+    width: 160px;
+    padding: 5px;
+    border-radius: 4px;
+    color: white;
+    background-image: linear-gradient(to bottom right, #ff6cab, #7366ff);
+
+}
+
+.button:hover {
+  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+}
+</style>
