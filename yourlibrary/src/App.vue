51520e3c4b597e21c2524cdbc9126437ac256ece
Josefine Magnussen <josefine.magnussen@hotmail.com>
2020-04-04 01:06:54 +0200

    updated

--- yourlibrary/src/App.vue
+++ yourlibrary/src/App.vue
@@ -1,23 +1,28 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <navbar/>
+    <div class="body">
+      <router-view/>
+    </div>
+    
   </div>
 </template>
 
 <script>
+import navbar from './components/navbar'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    navbar
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+}
+.body {
+  margin: 20px;
 }
 </style>
