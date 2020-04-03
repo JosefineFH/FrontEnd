23bb21617ba9722f4117569de1a43b493954601c
Josefine Magnussen <josefine.magnussen@hotmail.com>
2020-04-04 01:06:36 +0200

    navbar add

---
+++ yourlibrary/src/components/navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="navbar">
+    <h1>Test</h1>
+    <router-link to="login" > Login </router-link>
+        <router-link to="/" > Logout </router-link>
+
+  </div>
+</template>
+<script>
+export default {
+  
+};
+</script>
+<style scoped>
+.navbar {
+
+  font-family: sans-serif;
+  color: white;
+  background-image: linear-gradient(to bottom right, #ff6cab, #7366ff);
+    padding: 10px;
+}
+</style>
