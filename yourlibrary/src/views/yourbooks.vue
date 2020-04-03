1ce570a0904ad3b3af657e424136cadde324b0d0
Josefine Magnussen <josefine.magnussen@hotmail.com>
2020-04-04 01:05:23 +0200

    view for adding books

---
+++ yourlibrary/src/views/yourbooks.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1> Bøker </h1>
+        <ul>
+            <li v-for="post of posts" :key="post.id">
+                {{post.title}}
+            </li></ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            posts: []
+        }
+    },
+
+    created() {
+        axios
+        .get('http://localhost:3000/posts')
+        .then(response => {
+            this.posts = response.data
+            console.log(response.data);
+        })
+        .catch (function (error) {
+            console.log(error);
+        })
+    }
+}
+</script>
+
+<style>
+    
+</style>
