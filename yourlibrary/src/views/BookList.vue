a86dcea13d0d75f5e0ab6a9f268da677716619d9
Josefine Magnussen <josefine.magnussen@hotmail.com>
2020-04-16 01:21:20 +0200

    add showtotalItems with a true and false

--- yourlibrary/src/views/BookList.vue
+++ yourlibrary/src/views/BookList.vue
@@ -1,21 +1,25 @@
 <template>
   <div>
-    <h2>Add your books</h2>
-<p> Ther is {{totalItems}} total books</p>
+    <header class="main-header">
+      <h2>Search for your book</h2>
+      <p v-if="showTotalItems">
+        There is
+        <span>{{totalItems}}</span> total books in your search
+      </p>
+      <form @submit.prevent="search">
+        <input type="text" v-model="searchTerm" placeholder="søk" />
+        <button type="submit">search</button>
+      </form>
+    </header>
 
-<form @submit.prevent="search">
-    <input type="text" v-model="searchTerm" placeholder="søk" />
-    <button type="submit">search</button>
-</form>
-<div>
-  <BookCard v-for="book in searchResult.items" :key="book.id" :book="book"/>
-</div>
-
+    <section class="card">
+      <BookCard v-for="book in searchResult.items" :key="book.id" :book="book" />
+    </section>
   </div>
 </template>
 <script>
-import axios from "axios"
-import BookCard from '@/components/BookCard.vue'
+import axios from "axios";
+import BookCard from "@/components/BookCard.vue";
 
 export default {
   components: {
@@ -23,26 +27,48 @@ export default {
   },
   data() {
     return {
-      searchTerm: 'sarah j maas',
+      searchTerm: "sarah j maas",
       searchResult: [],
-      totalItems: ''
+      totalItems: "",
+      showTotalItems: false
     };
   },
 
   methods: {
     search() {
-       axios
-      .get("https://www.googleapis.com/books/v1/volumes?q=" + this.searchTerm)
-      .then(response => {
-        this.searchResult = response.data
-        this.totalItems = response.data.totalItems
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-    },
-  }
+      axios
+        .get("https://www.googleapis.com/books/v1/volumes?q=" + this.searchTerm)
+        .then(response => {
+          this.searchResult = response.data;
+          this.totalItems = response.data.totalItems;
+          // console.log(this.searchResult);
 
-  
+          if(this.totalItems !== '' ){
+            this.showTotalItems = true
+          }
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  }
 };
 </script>
+<style>
+.main-header {
+  height: 30vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: 1rm;
+}
+.main-header span {
+  color: #7366ff;
+}
+h2 {
+  font-size: 4rm;
+  line-height: 1.2;
+}
+</style>
