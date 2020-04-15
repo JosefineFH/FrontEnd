41ecf6f93ab12091ed18b374b965e9bb71387a49
Josefine Magnussen <josefine.magnussen@hotmail.com>
2020-04-16 00:25:06 +0200

    get info on your book

---
+++ yourlibrary/src/views/BookInfo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p>Book Info</p>
+
+    <p>{{book.title}}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  props: ["id"],
+  data() {
+    return {
+      book: {},
+      identifierOne: '',
+      identifierTwo: ''
+    };
+  },
+
+  created() {
+    axios
+      .get("https://www.googleapis.com/books/v1/volumes?q=" + this.id)
+      .then(response => {
+        this.book = response.data.items[0].volumeInfo;
+        // console.log(response.data.items[0].volumeInfo)
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+};
+</script>
+
+<style scoped>
+</style>
