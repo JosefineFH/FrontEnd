9645dd0f60a8129a79a468f5e26c9939e869b86f
Josefine Magnussen <josefine.magnussen@hotmail.com>
2020-04-16 00:26:40 +0200

    add bookcard
    title, author and img should appear here soon

---
+++ yourlibrary/src/components/BookCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-link :to="{name: 'BookInfo', params: { id:book.volumeInfo.industryIdentifiers[1].identifier  }}">
+    <div>
+      <p>{{book.volumeInfo.title }}</p>
+</div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    book: Object
+  }
+};
+</script>
+
+<style scoped>
+.bookCard {
+  background-color: green;
+}
+</style>
