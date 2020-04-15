a9b449fdc4ff531e657962b73c2c777f125925ea
Josefine Magnussen <josefine.magnussen@hotmail.com>
2020-04-16 01:20:27 +0200

    add css and img

--- yourlibrary/src/components/BookCard.vue
+++ yourlibrary/src/components/BookCard.vue
@@ -1,7 +1,8 @@
 <template>
-  <router-link :to="{name: 'BookInfo', params: { id:book.volumeInfo.industryIdentifiers[1].identifier  }}">
-    <div>
-      <p>{{book.volumeInfo.title }}</p>
+  <router-link :to="{name: 'BookInfo', params: { id:book.volumeInfo.industryIdentifiers[0].identifier  }}">
+    <div class="card">
+      <h3>{{book.volumeInfo.title }}</h3>
+ <img :src="'http://books.google.com/books/content?id=' + book.id + '&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'">
 </div>
   </router-link>
 </template>
@@ -15,7 +16,23 @@ export default {
 </script>
 
 <style scoped>
-.bookCard {
-  background-color: green;
+.card {
+      
+    justify-items: center;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 2rem;
+    background: #f1f1f1;
+    margin: 2rem 8rem 0rem 8rem;
+    padding: 1rem;
+  
 }
+
+.card h3 {
+        margin-bottom: 2rem;
+}
+.card img {
+    height: 300px;
+}
+
 </style>
