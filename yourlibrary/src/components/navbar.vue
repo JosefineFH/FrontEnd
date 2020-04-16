704ca941d1f42a20cef56a402235283f57f0dcb1
Josefine Magnussen <josefine.magnussen@hotmail.com>
2020-04-16 23:27:31 +0200

    working on a show and hide funkson

--- yourlibrary/src/components/navbar.vue
+++ yourlibrary/src/components/navbar.vue
@@ -1,27 +1,26 @@
 <template>
   <div class="navbar">
     <h1>Test</h1>
-    <router-link to="login" > Login </router-link>
-        <router-link to="login" > Logout </router-link>
+    <router-link v-if="ifLogedIn" to="/">Login</router-link>
+    <router-link v-if="!ifLogedIn" to="/">Logout</router-link>
 
-<router-link v-if="ifLogedIn" to="Books"> Books </router-link>
+    <router-link v-if="ifLogedIn" to="Bookshelf">Books</router-link>
   </div>
 </template>
 <script>
 export default {
-  data(){
-    return{
-      ifLogedIn: true,
-    }
-  }
+  data() {
+    return {
+      ifLogedIn: true
+    };
+  },
 };
 </script>
 <style scoped>
 .navbar {
-
   font-family: sans-serif;
   color: white;
   background-image: linear-gradient(to bottom right, #ff6cab, #7366ff);
-    padding: 10px;
+  padding: 10px;
 }
 </style>
