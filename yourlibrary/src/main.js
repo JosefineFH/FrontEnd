11a881cd57600560de2b6f04a3cadde3f6cff3df
Josefine Magnussen <josefine.magnussen@hotmail.com>
2020-04-28 22:37:24 +0200

    Removed

--- yourlibrary/src/main.js
+++ yourlibrary/src/main.js
@@ -6,7 +6,6 @@ import router from './router'
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
 
-Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
