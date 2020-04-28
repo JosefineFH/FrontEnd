11211bebeeb7a0868cc9f41acb3bd1b5640afca8
Josefine Magnussen <josefine.magnussen@hotmail.com>
2020-04-28 22:37:17 +0200

    Removed

--- yourlibrary/src/views/LoginUser.vue
+++ yourlibrary/src/views/LoginUser.vue
@@ -24,28 +24,13 @@ export default {
         email: "",
         password: ""
       },
-
       errors: ""
     };
   },
 
   methods: {
     login() {
-      const loginValues = {
-        email: this.loginInfo.email,
-        password: this.loginInfo.password
-      };
 
-      axios
-        .get("http://localhost:3000/users ", loginValues, {
-          withCredentials: true
-        })
-        .then(response => {
-          console.log(response);
-        })
-        .catch(function(e) {
-          console.log(e);
-        });
     }
   }
 };
@@ -71,34 +56,3 @@ form {
   box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
     0 17px 50px 0 rgba(0, 0, 0, 0.19);
 }
-</style>
-
-
-
- <!-- login(e) {
-      const loginValues = {
-        email: this.email,
-        password: this.password
-      };
-      axios
-        .get("http://localhost:3000/register", loginValues)
-        .then(response => {
-          this.data = response.data;
-          this.data.forEach(user => {
-            if (this.email != "" && this.password != "") {
-              if (user.email == this.email && user.password == this.password) {
-                this.$router.replace({ name: "Bookshelf" });
-                console.log(user);
-              } else {
-                console.log("The username and / or password is incorrect");
-              }
-            } else {
-              this.errors.push("A username and password must be present.");
-            }
-            e.preventDefault();
-          });
-        })
-        .catch(function(e) {
-          console.log(e);
-        });
-    } -->
