a7c883ad4572b1ea45d886eb56d9ff6856f07e03
Josefine Magnussen <josefine.magnussen@hotmail.com>
2020-04-28 22:00:48 +0200

    removed vuex from component

--- EventApp/src/views/EventShow.vue
+++ EventApp/src/views/EventShow.vue
@@ -32,17 +32,35 @@
   </div>
 </template>
 <script>
-import { mapState, mapActions } from 'vuex'
+// import { mapState } from 'vuex'
+// import NProgress from 'nprogress'
+// import store from '@/store/store'
 
 export default {
-  props: ['id'],
-  created() {
-    this.fetchEvent(this.id)
-  },
-  computed: mapState({
-    event: state => state.event.event
-  }),
-  methods: mapActions('event', ['fetchEvent'])
+  props: {
+    event: {
+      //reveive event to render
+      type: Object,
+      required: true
+    }
+  }
+  // props: ['id'],
+  // computed: mapState({
+  //   event: state => state.event.event
+  // })
+  // beforeRouteEnter(routeTo, routeFrom, next) {
+  //   NProgress.start()
+  //   store.dispatch('event/fetchEvent', routeTo.params.id).then(() => {
+  //     NProgress.done()
+  //     next()
+  //   })
+  // },
+
+  // created() {
+  //   this.fetchEvent(this.id)
+  // },
+ 
+  // methods: mapActions('event', ['fetchEvent'])
 }
 </script>
 <style scoped>
