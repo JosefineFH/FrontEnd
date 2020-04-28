19ce6c18a2356b3f1ac88fae07bf5e91a77ec8de
Josefine Magnussen <josefine.magnussen@hotmail.com>
2020-04-28 22:00:12 +0200

    added progress bar
    reouter.beforeEach()
    add beforeEach in eventShow route.
    reouter.afterEach()

--- EventApp/src/router.js
+++ EventApp/src/router.js
@@ -3,10 +3,12 @@ import Router from 'vue-router'
 import EventList from './views/EventList.vue'
 import EventShow from './views/EventShow.vue'
 import EventCreate from './views/EventCreate'
+import NProgress from 'nprogress'
+import store from '@/store/store'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
@@ -18,7 +20,14 @@ export default new Router({
       path: '/event/:id',
       name: 'event-show',
       component: EventShow,
-      props: true
+      props: true,
+      beforeEnter(routeTo, routeFrom, next) {
+        // before this route is loaded
+        store.dispatch('event/fetchEvent', routeTo.params.id).then(event => {
+          routeTo.params.event = event // <--- Set the event we retrieved
+          next()
+        })
+      }
     },
     {
       path: '/event/create',
@@ -27,3 +36,16 @@ export default new Router({
     }
   ]
 })
+
+router.beforeEach((routerTo, routerFrom, next) => {
+  //start the progress bar.
+  NProgress.start()
+  next()
+})
+
+router.afterEach(() => {
+  // complete the animation of the router progress bar.
+  NProgress.done()
+})
+
+export default router
