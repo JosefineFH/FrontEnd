9690c8af90675402c7f1c5da5032d34e05fe6f73
Josefine Magnussen <josefine.magnussen@hotmail.com>
2020-05-12 12:02:26 +0200

    created error vue

--- EventApp/src/views/NotFound.vue
+++ EventApp/src/views/NotFound.vue
@@ -1,13 +1,16 @@
 <template>
     <div>
         <h1>Oops!</h1>
-        <h3>The page you're looking for is not here.</h3>
+        <h3>The 
+        {{resource}}
+        page you're looking for is not here.
+        </h3>
         <router-link :to="{name: 'event-list'}"> Back to the home page</router-link>
     </div>
 </template>
 <script>
 export default {
-  prop: {
+  props: {
     resource: {
       type: String,
       required: true
@@ -15,3 +18,4 @@ export default {
   }
 }
 </script>
+
