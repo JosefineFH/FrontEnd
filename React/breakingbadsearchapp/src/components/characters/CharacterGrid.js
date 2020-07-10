f420f7b08078931971d720d9a3780ff9d5259d27
Josefine Magnussen <josefine.magnussen@hotmail.com>
2020-07-10 11:46:13 +0200

    Added

---
+++ React/breakingbadsearchapp/src/components/characters/CharacterGrid.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CharacterItem from './CharacterItem';
+
+const CharacterGrid = ({ item, isLoading }) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CharacterGrid
